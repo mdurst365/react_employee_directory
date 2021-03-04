82c9055fb6199a41cfed9b1d6d7b458a2108ccae
Molly Durst <mollymadegraphics@yahoo.com>
2021-03-04 18:57:08 -0500

    creating initial index.js and App.js files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+//imports
+import React from "react";
+//import container component and any other needed components here
+
+function App() {
+    //this will return a container
+  }
+
+//export
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+//import React
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//React.render
+ReactDOM.render(<App />, document.getElementById('root'));
