d37e8e2d416de068035e55cac1b58458d94856a0
Molly Durst <mollymadegraphics@yahoo.com>
2021-03-04 19:14:15 -0500

    setting up App.js to recieve components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 //imports
 import React from "react";
 //import container component and any other needed components here
+import headerInfo from "./components/headerInfo";
+import usersContainer from "./components/usersContainer";
+import footerInfo from "./components/footerInfo"
 
 function App() {
-    //this will return a container
+    //this will return the containers
+    return<>
+    <headerInfo/>
+    <usersContainer/>
+    <footerInfo/>
+    </>
   }
 
 //export
