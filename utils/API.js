7c9acb7a1ba76758c3514a819327a566bebf7330
Molly Durst <mollymadegraphics@yahoo.com>
2021-03-04 19:08:25 -0500

    added API with call for user list

--- utils/API.js
+++ utils/API.js
@@ -1,4 +1,11 @@
 //import data from the API
 import axios from "axios";
 
+function usersList() {
+    return axios.get("https://randomuser.me/api/?results=100&nat=US");
+  }
+
 //export
+export default {
+    usersList
+  };
