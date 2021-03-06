58e2b0b9b776c1e72e54b2cf20d2594766a3552c
Molly Durst <mollymadegraphics@yahoo.com>
2021-03-05 19:32:00 -0500

    Updates

--- src/components/footerInfo.js
+++
@@ -1,3 +0,0 @@
-<div class="container-fluid .bg-secondary">
-  Created as a homework assignment for the OSU Coding Bootcamp &bull; MIT License &bull; All Rights Reserved
-</div>
--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-//import data from the API
-import axios from "axios";
-
-//export
-export default {
-    usersList: function () {
-      return axios.get("https://randomuser.me/api/?results=25&nat=US"); 
-    }
-  };
